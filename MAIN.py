2d68b43b6f7218c40b53f64335234a0aa112fa09
Dario Woloszin <3h2so4@gmail.com>
2024-05-04 15:27:14 -0300

    updates

--- MAIN.py
+++ MAIN.py
@@ -13,56 +13,60 @@ import distCalc
 import distanceT
 import GOOGLE_CREATOR
 import timeit
-inicioTotal = timeit.default_timer()
-
-#timeexport = time.strftime("%Y%m%d_")
-script_dir = os.path.abspath(os.path.dirname(sys.argv[0]) or '.')
-
-
-csv_path = os.path.join(script_dir, 'export/'+'AnatelBase'+'.csv')
-zip_path = os.path.join(script_dir, 'export/'+'AnatelBase'+'.zip')
-
-#Download All files
-AnatelFiles.download('SP',['GSM'])
-#AnatelFiles.download('SP',['GSM', 'WCDMA', 'LTE', 'NR'])
 
 
 
+if __name__ == "__main__":
+    inicioTotal = timeit.default_timer()
 
-# Example usage:
-#zip_directory = 'E:\GoogleDriveOnline\PYTHON\OperadorasAnatel\import\BaseAnatel'
-zip_directory = os.path.join(script_dir, 'import/'+'Baseanatel')
-df = ImportDF.ImportDFFromZip(zip_directory)
- 
-
-df.drop_duplicates(inplace=True, ignore_index=True)
-
-df = CleanData.process(df)
-
-'''
-#==========================================
-#run once a month
-opList = ['TIM','CLARO','VIVO','ALGAR']
-for i in opList:
-    distCalc.process(i)
-#=======================================
-'''
+    #timeexport = time.strftime("%Y%m%d_")
+    script_dir = os.path.abspath(os.path.dirname(sys.argv[0]) or '.')
 
 
-df = distanceT.process(df)
+    csv_path = os.path.join(script_dir, 'export/'+'AnatelBase'+'.csv')
+    zip_path = os.path.join(script_dir, 'export/'+'AnatelBase'+'.zip')
 
-
-df.to_csv(csv_path, sep=',',encoding='ANSI', index=False)  # Save DataFrame as CSV
-time.sleep(10)
-Csv_zip.process(csv_path)
+    #Download All files
+    AnatelFiles.download('SP',['GSM'])
+    #AnatelFiles.download('SP',['GSM', 'WCDMA', 'LTE', 'NR'])
 
 
 
-GOOGLE_CREATOR.process()
+
+    # Example usage:
+    #zip_directory = 'E:\GoogleDriveOnline\PYTHON\OperadorasAnatel\import\BaseAnatel'
+    zip_directory = os.path.join(script_dir, 'import/'+'Baseanatel')
+    df = ImportDF.ImportDFFromZip(zip_directory)
+    
+
+    df.drop_duplicates(inplace=True, ignore_index=True)
+
+    df = CleanData.process(df)
+
+    '''
+    #==========================================
+    #run once a month
+    opList = ['TIM','CLARO','VIVO','ALGAR']
+    for i in opList:
+        distCalc.process(i)
+    #=======================================
+    '''
+
+
+    df = distanceT.process(df)
+
+
+    df.to_csv(csv_path, sep=',',encoding='ANSI', index=False)  # Save DataFrame as CSV
+    time.sleep(10)
+    Csv_zip.process(csv_path)
 
 
 
-fimtotal = timeit.default_timer()
-print ('duracao: %f' % round(((fimtotal - inicioTotal)/60),2) + ' min') 
-print ('\nALL_DONE!!!')
+    GOOGLE_CREATOR.process()
+
+
+
+    fimtotal = timeit.default_timer()
+    print ('duracao: %f' % round(((fimtotal - inicioTotal)/60),2) + ' min') 
+    print ('\nALL_DONE!!!')
 
